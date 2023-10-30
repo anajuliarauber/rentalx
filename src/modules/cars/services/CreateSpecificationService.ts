f275112efcfb8f2ed93c0687c9582b680afe069f
anajuliarauber <anajuliarauber@hotmail.com>
2023-10-30 20:00:52 -0300

    feat: add create specification

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,25 @@
+import { type ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor(
+    private readonly specificationsRepository: ISpecificationsRepository,
+  ) {}
+
+  execute({ description, name }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists!");
+    }
+
+    this.specificationsRepository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationService };
