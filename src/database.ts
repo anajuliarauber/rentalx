41805b1f7ba14627c3f80d954b3a0360cf74c9dd
anajuliarauber <anajuliarauber@hotmail.com>
2024-02-19 14:24:52 -0300

    feat: add postgres connection

---
+++ src/database.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "ignite",
+  password: "ignite",
+  database: "rentalx",
+  entities: ["./src/modules/**/entities/*.ts"],
+  migrations: ["./src/migrations/*.ts"],
+});
