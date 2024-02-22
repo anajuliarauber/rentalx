b94b88c8327665e5e349889565f5736a5e23da99
anajuliarauber <anajuliarauber@hotmail.com>
2024-02-22 15:52:23 -0300

    refactor: separate upload config to its own file

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import multer from "multer";
+import uploadConfig from "../config/upload";
 
 import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import { ImportCategoriesController } from "../modules/cars/useCases/importCategory/ImportCategoriesController";
 import { ListCategoriesController } from "../modules/cars/useCases/listCategories/ListCategoriesController";
 
 const categoriesRoutes = Router();
-const upload = multer({
-  dest: "./tmp",
-});
+
+const uploadCategories = multer(uploadConfig.upload("./tmp"))
 
 const createCategoryController = new CreateCategoryController();
 const importCategoriesController = new ImportCategoriesController()
@@ -18,5 +18,5 @@ categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.get("/", listCategoriesController.handle);
 
-categoriesRoutes.post("/import", upload.single("file"), importCategoriesController.handle);
+categoriesRoutes.post("/import", uploadCategories.single("file"), importCategoriesController.handle);
 export { categoriesRoutes };
