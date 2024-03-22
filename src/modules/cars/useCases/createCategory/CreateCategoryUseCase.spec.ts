33dd3b3632ceaa2ac570ee8d252072b6af3406b8
anajuliarauber <anajuliarauber@hotmail.com>
2024-03-22 13:16:12 -0300

    feat: create first test

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,7 @@
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+describe("Create category", () => {
+  it("should be able to create a new category", () => {
+    const createCategory = new CreateCategoryUseCase();
+  });
+});
